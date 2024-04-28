c4e10d22f9e6e80777e537bcb46a129a287024b6
JoseMiguelContreras <144606956+JoseMiguelContreras@users.noreply.github.com>
2024-04-28 18:10:39 -0400

    se agrega el proyecto

---
+++ 100 años.py
@@ -0,0 +1,6 @@
+nombre = input("Escribe tu nombre")
+edad = int(input("Escribe tu edad"))
+
+anios_cumplidos = (100 - edad) + 2024
+
+print("hola", nombre, "Cumpliras 100 años en el ano", anios_cumplidos)
